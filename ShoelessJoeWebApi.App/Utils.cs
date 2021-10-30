4a7fac688b01e2c3ef9e5e7507c5c1cf22432f04
claypooltyler00 <claypooltyler00@gmail.com>
2021-10-30 18:33:06 -0500

    File upload now works as it should

--- ShoelessJoeWebApi.App/Utils.cs
+++ ShoelessJoeWebApi.App/Utils.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
-using ShoelessJoeWebApi.App.ApiModels;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ShoelessJoeWebApi.App
@@ -16,19 +16,44 @@ namespace ShoelessJoeWebApi.App
         public static string UniqueFileName(string fileName)
         {
             string withoutExt = Path.GetFileNameWithoutExtension(fileName);
-            string ext = Path.GetExtension(fileName);
 
-            return withoutExt + Guid.NewGuid() + ext;
+            return withoutExt + Guid.NewGuid() + Path.GetExtension(fileName);
         }
 
         public async static Task<string> FileUpload(IFormFile file, string shoeFolder)
         {
             string uniqueFileName = UniqueFileName(file.FileName);
+
+            if (!IsJpeg(file))
+            {
+                using var memoryStream = new MemoryStream();
+                await file.CopyToAsync(memoryStream);
+
+                using Image image = Image.FromStream(memoryStream);
+                image.Save(uniqueFileName, ImageFormat.Jpeg);
+            }
+
             using var stream = new FileStream($@"{shoeFolder}\{uniqueFileName}", FileMode.Create);
             await file.CopyToAsync(stream);
 
             return uniqueFileName;
+        }
 
+        public static string FormatFileName(string fileName, int userId, int shoeId)
+        {
+            return $"{ServerUrl}user{userId}/shoe{shoeId}/{fileName}";
+        }
+
+        private static bool IsJpeg(IFormFile file)
+        {
+            string ext = Path.GetExtension(file.FileName);
+
+            if (ext.Contains("jpg") || ext.Contains("JPG") || ext.Contains("jpeg"))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
